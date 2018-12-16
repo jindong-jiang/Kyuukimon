7c961e12ee85f98e061db5bbddaf1ee9ff2fe95e
Jindong <jiangjindongkyle@gmail.com>
2018-12-16 23:22:12 +0800

    plot #temp

--- AddtiveAdapterExpSimult.py
+++ AddtiveAdapterExpSimult.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 currentDir = os.path.dirname(__file__)
 tempDir = os.path.join(currentDir, 'tempDeNOx')
-
-for GasCompent in [[0.34,0.23,0.31,0.105],[0.27,0.64,0.03,0.001]]:
+#[0.27,0.64,0.03,0.001]
+for GasCompent in [[0.34,0.23,0.31,0.105],]:
     NO_EndPoint_Detail_Temp=[]
     correctionCoefiencts=0.8
     concentrationList=np.array([0,300e-6,900e-6])*correctionCoefiencts
@@ -49,10 +49,34 @@ for GasCompent in [[0.34,0.23,0.31,0.105],[0.27,0.64,0.03,0.001]]:
 
     NO_EndPoint_Detail_Temp_Np=np.array(NO_EndPoint_Detail_Temp)
 
-    df=pd.DataFrame(data={'temperature':temperatureListX,'NO_Detail':NO_EndPoint_Detail_Temp_Np})          
-    df.to_csv("DataAnalyse\\YangmeiGas\\simulationData.csv")
+    df=pd.DataFrame(data={'temperature':temperatureListX})
+    df1=pd.DataFrame(data={'NO_Detail':NO_EndPoint_Detail_Temp_Np})   
+    dftoWrite=pd.concat([df,df1],axis=1)       
+    dftoWrite.to_csv("DataAnalyse\\YangmeiGas\\simulationData.csv")
     C_NO_Detail=NO_EndPoint_Detail_Temp_Np.reshape(len(concentrationList),len(temperatureListX))
     plt.figure()
     for dataLine in C_NO_Detail:        
-        plt.plot(temperatureListX,dataLine,'-')
+        plt.plot(temperatureListX-273.15,dataLine,'-')
+    
+    data={}
+    with pd.ExcelFile(r'DataAnalyse\Additive\data1ResultSynGasSNCR1.xlsx') as xls:
+        data['exp_1_900'] = pd.read_excel(xls, '合成气1,900ppm ',usecols=[0, 2])
+        data['smlt_1_900']=pd.read_excel(xls, '合成气1,900ppm ',usecols=[3, 5])
+        data['exp_1_300'] = pd.read_excel(xls, '合成气1,300ppm',usecols=[0, 2])
+        data['smlt_1_300']=pd.read_excel(xls, '合成气1,300ppm',usecols=[4, 6])
+        data['exp_1_000'] = pd.read_excel(xls, 'NSR1.5,t0.6s',usecols=[0, 2])
+        data['smlt_1_000']=pd.read_excel(xls, 'NSR1.5,t0.6s',usecols=[3, 5])
+        
+          
+        symbol=iter(['s','--','^',':','*','-.'])
+        for datatoplot in data:
+            plt.plot(data[datatoplot].iloc[:,0],data[datatoplot].iloc[:,1],next(symbol))
+        #plt.xlim((550,1100))
+        plt.xlabel('Temperature ($^\circ$C)',fontsize="medium")
+        plt.ylabel('[NO](out)/[NO](in)',fontsize="medium")
+        plt.legend(['Simulation additive#1 900μL/L','Experiment additive#1 900μL/L',
+                    'Simulation additive#1 300μL/L','Experiment additive#1 300μL/L',
+                    'Simulation additive#1 0μL/L','Experiment additive#1 0μL/L'],fontsize="medium")
+        #plt.savefig("DataAnalyse\\Fig\\syngasYang1.png",bbox_inches='tight')
+    
     plt.show()
